000bb8d7565e8599c188808e3c8529788926454d
fouqau4 <fouqau4@gmail.com>
2020-05-03 14:09:33 +0800

    A prototype of shell script which can send LINE message by curl.

---
+++ line.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source tokenfile
+
+token=${token_ds}
+
+for i in `seq 1 5`;
+do
+    curl -s -X POST \
+     -H "Authorization: Bearer ${token}" \
+     -F 'message=TEST' https://notify-api.line.me/api/notify
+done
+
+# Reference
+# - https://engineering.linecorp.com/zh-hant/blog/using-line-notify-to-send-messages-to-line-from-the-command-line/
+# - https://notify-bot.line.me/my/
